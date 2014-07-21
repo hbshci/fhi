daf29502b189cd032f0c3a13632856773b821bbd
Myer Nore <ptr.nore@gmail.com>
2014-07-21 17:06:42 -0400

    cleaned out directory to prepare for gh-pages

--- content/challenge/case-written-about-your-organization.md
+++
@@ -1,3 +0,0 @@
-### HBS Case Study
-
-As a finalist you will be paired with renowned health care faculty who will write a Harvard Business School case study about your organization to be discussed at the conference and potentially taught in class rooms around the world. Faculty writers include:
--- data/_utils/extend-pkg.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  repo: "git@github.com:codekiln/fhi.git",
-  issues: "https://github.com/codekiln/fhi/issues",
-  master: "https://github.com/codekiln/fhi/blob/master",
-  rawmaster: "https://raw.github.com/codekiln/fhi/master/dist",
-  sourcearchive: "https://github.com/codekiln/fhi/archive/v"
-};
